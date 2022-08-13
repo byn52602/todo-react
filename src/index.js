262139fec8a66ee9537d16ee6237dcd7fe338729
Yani Bu <yanibu@Yanis-MacBook-Pro.local>
2022-08-13 15:02:27 -0400

    made necessary changes to deploy on gh-pages

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react"
 import { createRoot } from 'react-dom/client'
 import TodoContainer from "./functionBased/components/TodoContainer"
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 import "./functionBased/app.css"
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <TodoContainer />
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>
 )
