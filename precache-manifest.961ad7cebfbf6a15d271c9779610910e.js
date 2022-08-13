9cf94983a6cbba6429ef733467dde67e65b1e3ad
Yani Bu <yanibu@Yanis-MacBook-Pro.local>
2022-08-13 15:10:05 -0400

    Updates

---
+++ precache-manifest.961ad7cebfbf6a15d271c9779610910e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4bba95d9646494daf125",
+    "url": "/todo-react/static/css/main.7cf4ecc8.chunk.css"
+  },
+  {
+    "revision": "4bba95d9646494daf125",
+    "url": "/todo-react/static/js/main.63cb41b4.chunk.js"
+  },
+  {
+    "revision": "cd203911fc85a7f73b39",
+    "url": "/todo-react/static/js/runtime~main.6292bb99.js"
+  },
+  {
+    "revision": "c382ae4f249589b18b6d",
+    "url": "/todo-react/static/js/2.9a6be03b.chunk.js"
+  },
+  {
+    "revision": "01bb7804f1c56b5306bc51ef8f96e786",
+    "url": "/todo-react/index.html"
+  }
+];
